8acc761d69996f8fdb6ee53dcb9e921aca60ce99
Herman Karlsson <herkarlsson@gmail.com>
2021-11-16 20:30:29 +0100

    Add database definition

---
+++ database/models.sql
@@ -0,0 +1,67 @@
+CREATE TABLE Elections (
+    id              TEXT NOT NULL,
+    name            TEXT NOT NULL,
+    published       BOOLEAN NOT NULL,
+    openTime        TIMESTAMP,
+    closeTime       TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Valid_Voters (
+    username        TEXT NOT NULL,
+    PRIMARY KEY(username)
+);
+
+CREATE TABLE Candidates (
+    id              TEXT NOT NULL,
+    name            TEXT NOT NULL,
+    presentation    TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE Casted_Votes (
+    username        TEXT NOT NULL,
+    electionID      TEXT NOT NULL,
+    PRIMARY KEY(username, electionID),
+
+    CONSTRAINT fk_voters
+        FOREIGN KEY(username)
+        REFERENCES Valid_Voters(username),
+    CONSTRAINT fk_elections
+        FOREIGN KEY(electionID)
+        REFERENCES Elections(id)
+);
+
+CREATE TABLE Votes (
+    hash            TEXT NOT NULL,
+    electionID      TEXT NOT NULL,
+    isBlank         BOOLEAN NOT NULL,
+    PRIMARY KEY(hash),
+
+    CONSTRAINT fk_elections
+        FOREIGN KEY(electionID)
+        REFERENCES Elections(id)
+);
+
+CREATE TABLE Candidates_In_Elections (
+    electionID      TEXT NOT NULL,
+    candidateID     TEXT NOT NULL,
+    PRIMARY KEY (electionID, candidateID),
+
+    CONSTRAINT fk_elections
+        FOREIGN KEY(electionID)
+        REFERENCES Elections(id),
+    CONSTRAINT fk_candidates
+        FOREIGN KEY(candidateID)
+        REFERENCES Candidates(id)
+);
+
+CREATE TABLE Vote_Log (
+    VoteHash        TEXT NOT NULL,
+    VoteTime        TIMESTAMP NOT NULL,
+    PRIMARY KEY(VoteHash),
+
+    CONSTRAINT fk_votes
+        FOREIGN KEY(VoteHash)
+        REFERENCES Votes(hash)
+)

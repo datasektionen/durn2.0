30e2d495d62acffac54b111317ca8cbc6a2c5975
Herman Karlsson <herkarlsson@gmail.com>
2021-11-18 01:06:53 +0100

    Add some initial queries

--- database/db.go
+++ database/db.go
@@ -2,12 +2,15 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"sync"
 
+	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 
 	"durn2.0/conf"
+	"durn2.0/durn"
 )
 
 var mutex sync.Mutex
@@ -33,14 +36,14 @@ func DisconnectDB() {
 	db.Close()
 }
 
-func QueryAllVoters() []string {
+func QueryAllVoters() ([]string, error) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
 	query := "SELECT * from valid_voters"
 	rows, err := db.Query(query)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer rows.Close()
 	var res []string
@@ -51,5 +54,38 @@ func QueryAllVoters() []string {
 		res = append(res, username)
 	}
 
-	return res
+	return res, nil
 }
+
+func InsertElection(durn.Election e) error {
+	query := "INSERT INTO Elections(id, name, published, finalized, opentime, closetime) values ($1, $2, $3, $4, $5, $6)"
+	_, err := db.Exec(query, e.id, e.name, e.isOpen, e.isFinalized, e.openTime, e.closeTime)
+	if err != nil {
+		println(err) // TODO: better logging
+		return errors.New("Failure while inserting into Elections, see logs for more info")
+	}
+	return nil
+}
+
+func InsertCandidate(durn.Election e) error {
+	query := "INSERT INTO Candidates(id, name, presentation) VALUES ($1, $2, $3)"
+	_, err := db.Exec(query, e.id, e.name, e.presentation)
+	if err != nil {
+		println(err) // TODO: better logging
+		return errors.New("Failure while inserting into Candidates, see logs for more info")
+	}
+	return nil
+}
+
+func UpdateElectionInfo(durn.Election e) error {
+	query := "SELECT * from where id = $1"
+
+	row, err := db.QueryRow(query, e.id)
+
+
+
+	query = "UPDATE where id = $1"
+	_, err = db.Exec(query, e.id)
+}
+
+func

6d1a693afef934b114028c217de2eea8bf8c8c5c
Herman Karlsson <herkarlsson@gmail.com>
2022-01-07 17:26:48 +0100

    Implent database loading using gorm
    As well ass commment out all the old database functionality that should not be used

--- database/db.go
+++ database/db.go
@@ -1,61 +1,71 @@
 package database
 
 import (
-	"database/sql"
-	"errors"
 	"fmt"
 	"sync"
 
 	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"durn2.0/conf"
 	"durn2.0/models"
 )
 
 var mutex sync.Mutex
-var db *sql.DB
+var db *gorm.DB
 
 // CreateDBConnection Initializes the database connection using
 // the connection details specified in env-vars
-func CreateDBConnection() {
+func CreateDBConnection() error {
 	c := conf.ReadConfiguration()
-	psqlconn := fmt.Sprintf("host=localhost port=%d user='%s' password='%s' dbname='%s' sslmode=disable", c.DBPort, c.DBUser, c.DBPassword, c.DBName)
-
 	mutex.Lock()
 	defer mutex.Unlock()
-	var err error
-	db, err = sql.Open("postgres", psqlconn)
+
+	dsn := fmt.Sprintf("host=%s port=%d user='%s' password='%s' dbname='%s' sslmode=disable", c.DBHost, c.DBPort, c.DBUser, c.DBPassword, c.DBName)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		return err
 	}
+	database.AutoMigrate(&Election{})
+	database.AutoMigrate(&Valid_Voter{})
+	database.AutoMigrate(&Candidate{})
+	database.AutoMigrate(&Casted_Vote{})
+	database.AutoMigrate(&Vote{})
+
+	db = database
+	return nil
 }
 
-func DisconnectDB() {
+func TakeDB() *gorm.DB {
 	mutex.Lock()
-	defer mutex.Unlock()
+	return db
+}
 
-	db.Close()
+func ReleaseDB() {
+	mutex.Unlock()
 }
 
 func QueryAllVoters() ([]models.Voter, error) {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	query := `SELECT * FROM valid_voters`
-	rows, err := db.Query(query)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-	var res []models.Voter
+	// query := `SELECT * FROM valid_voters`
+	// rows, err := db.Query(query)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// defer rows.Close()
+	// var res []models.Voter
 
-	for rows.Next() {
-		var username models.Voter
-		err = rows.Scan(&username)
-		res = append(res, username)
-	}
+	// for rows.Next() {
+	// 	var username models.Voter
+	// 	err = rows.Scan(&username)
+	// 	res = append(res, username)
+	// }
 
-	return res, nil
+	// return res, nil
+	return nil, nil
 }
 
 // InsertVoters inserts provided voters into the database, but first queries the database
@@ -64,28 +74,30 @@ func InsertVoters(voters []models.Voter) error {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	query := `SELECT * FROM valid_voters`
-	rows, err := db.Query(query)
-	if err != nil {
-		return err
-	}
+	// db.create()
 
-	alreadyAdded := map[models.Voter]bool{}
-	for rows.Next() {
-		var voter models.Voter
-		if err := rows.Scan(&voter); err != nil {
-			return err
-		}
-		alreadyAdded[voter] = true
-	}
+	// query := `SELECT * FROM valid_voters`
+	// rows, err := db.Query(query)
+	// if err != nil {
+	// 	return err
+	// }
 
-	for _, voter := range voters {
-		if !alreadyAdded[voter] {
-			if _, err = db.Exec(`INSERT INTO valid_voters VALUES ($1)`, voter); err != nil {
-				return err
-			}
-		}
-	}
+	// alreadyAdded := map[models.Voter]bool{}
+	// for rows.Next() {
+	// 	var voter models.Voter
+	// 	if err := rows.Scan(&voter); err != nil {
+	// 		return err
+	// 	}
+	// 	alreadyAdded[voter] = true
+	// }
+
+	// for _, voter := range voters {
+	// 	if !alreadyAdded[voter] {
+	// 		if _, err = db.Exec(`INSERT INTO valid_voters VALUES ($1)`, voter); err != nil {
+	// 			return err
+	// 		}
+	// 	}
+	// }
 
 	return nil
 }
@@ -94,12 +106,12 @@ func DeleteVoters(voters []models.Voter) error {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	for _, voter := range voters {
-		query := `DELETE FROM valid_voters WHERE email = $1`
-		if _, err := db.Exec(query, voter); err != nil {
-			return err
-		}
-	}
+	// for _, voter := range voters {
+	// 	query := `DELETE FROM valid_voters WHERE email = $1`
+	// 	if _, err := db.Exec(query, voter); err != nil {
+	// 		return err
+	// 	}
+	// }
 
 	return nil
 }
@@ -108,12 +120,12 @@ func InsertElection(e models.Election) error {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	query := `INSERT INTO Elections(id, name, published, finalized, opentime, closetime) VALUES ($1, $2, $3, $4, $5, $6)`
-	_, err := db.Exec(query, e.Id, e.Name, e.IsOpen, e.IsFinalized, e.OpenTime, e.CloseTime)
-	if err != nil {
-		println(err)
-		return errors.New("Failure while inserting into Elections, see logs for more info")
-	}
+	// query := `INSERT INTO Elections(id, name, published, finalized, opentime, closetime) VALUES ($1, $2, $3, $4, $5, $6)`
+	// _, err := db.Exec(query, e.Id, e.Name, e.IsOpen, e.IsFinalized, e.OpenTime, e.CloseTime)
+	// if err != nil {
+	// 	println(err)
+	// 	return errors.New("Failure while inserting into Elections, see logs for more info")
+	// }
 	return nil
 }
 
@@ -121,10 +133,10 @@ func InsertCandidate(e models.Candidate) error {
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	query := `INSERT INTO Candidates(id, name, presentation) VALUES ($1, $2, $3)`
-	if _, err := db.Exec(query, e.Id, e.Name, e.Presentation); err != nil {
-		println(err)
-		return errors.New("Failure while inserting into Candidates, see logs for more info")
-	}
+	// query := `INSERT INTO Candidates(id, name, presentation) VALUES ($1, $2, $3)`
+	// if _, err := db.Exec(query, e.Id, e.Name, e.Presentation); err != nil {
+	// 	println(err)
+	// 	return errors.New("Failure while inserting into Candidates, see logs for more info")
+	// }
 	return nil
 }

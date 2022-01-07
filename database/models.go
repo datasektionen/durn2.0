8cea8e92c6b2fd0902a33b526bbdeb590e662792
Herman Karlsson <herkarlsson@gmail.com>
2022-01-07 15:58:10 +0100

    rewrk models

--- database/models.go
+++ database/models.go
@@ -1,93 +1,44 @@
 package database
 
 import (
+	"database/sql"
 	"time"
 
-	// "github.com/google/uuid"
 	"github.com/google/uuid"
-	"github.com/jinzhu/gorm"
 )
 
 type Election struct {
-	gorm.Model
-	ID         uuid.UUID `gorm:"primaryKey"`
-	Name       string    `gorm:"not null"`
-	published  bool      `gorm:"not null"`
-	finalized  bool      `gorm:"not null"`
-	openTime   time.Time
-	closeTime  time.Time
-	Candidates []Candidate `gorm:"many2many:candidate_in_election"`
-	Votes      []Vote
+	ID         uuid.UUID    `gorm:"primaryKey"`
+	Name       string       `gorm:"not null"`
+	Published  bool         `gorm:"not null"`
+	Finalized  bool         `gorm:"not null"`
+	OpenTime   sql.NullTime ``
+	CloseTime  sql.NullTime ``
+	Candidates []Candidate  `gorm:"many2many:candidate_in_election"`
+	Votes      []Vote       ``
 }
 
 type Valid_Voter struct {
-	gorm.Model
-	email string `gorm:"primaryKey"`
+	Email string `gorm:"primaryKey"`
 }
 
 type Candidate struct {
-	gorm.Model
 	ID           uuid.UUID `gorm:"primaryKey"`
 	Name         string    `gorm:"not null"`
-	presentation string    `gorm:"not null"`
+	Presentation string    `gorm:"not null"`
 }
 
 type Casted_Vote struct {
-	gorm.Model
-	voter    Valid_Voter `gorm:"primaryKey"`
-	election Election    `gorm:"primaryKey"`
+	VoterID    uuid.UUID   `gorm:"primaryKey"`
+	ElectionID uuid.UUID   `gorm:"primaryKey"`
+	Voter      Valid_Voter `gorm:"foreignKey:VoterID"`
+	Election   Election    `gorm:"foreignKey:ElectionID"`
 }
 
 type Vote struct {
-	gorm.Model
-	hash     string    `gorm:"primaryKey"`
-	election Election  `gorm:"not null"`
-	isBlank  bool      `gorm:"not null"`
-	voteTime time.Time `gorm:"not null"`
+	Hash       string    `gorm:"primaryKey"`
+	IsBlank    bool      `gorm:"not null"`
+	VoteTime   time.Time `gorm:"not null"`
+	ElectionID uuid.UUID `gorm:"not null"`
+	Election   Election  `gorm:"foreignKey:ElectionID"`
 }
-
-// import (
-// 	"time"
-
-// 	"github.com/google/uuid"
-// 	// _ "go.mongodb.org/mongo-driver/bson"
-// )
-
-// type Election struct {
-// 	id        uuid.UUID `bson:id`
-// 	name      string    `bson:name`
-// 	published bool      `bson:published`
-// 	openTime  time.Time `bson:`
-// 	closeTime time.Time
-// }
-
-// type ValidVoter struct {
-// 	mail string
-// }
-
-// type Candidate struct {
-// 	id           uuid.UUID
-// 	name         string
-// 	presentation string
-// }
-
-// type CastedVote struct {
-// 	mail       string
-// 	electionId uuid.UUID
-// }
-
-// type Vote struct {
-// 	hash       string
-// 	electionId uuid.UUID
-// 	isBlank    bool
-// }
-
-// type CandidateInElection struct {
-// 	electionId  uuid.UUID
-// 	CandidateId uuid.UUID
-// }
-
-// type VoteEvent struct {
-// 	voteId   string
-// 	voteTime time.Time
-// }
